93d3feb55d55c0336f3027bdda5bbfa9170fbfab
johnnsab <sabranjohn@gmail.com>
2023-03-15 14:33:00 +0200

    review fixes

--- homework-5/script.js
+++ homework-5/script.js
@@ -1,16 +1,19 @@
+let arr = ["a", 2, { a: 1 }, ["a", "b"], 1, 9.3, "asdf", true, 8, null, 10];
+
 // Given an array with elements of different types. Create a function that calculates the arithmetic mean of only the numerical elements of the given array.
 
-function sumNumbers() {
-  let data = prompt("Enter array").split(",");
+function sumNumbers(data) {
   let sum = 0;
+  let count = 0;
   for (let i = 0; i < data.length; i++) {
-    let num = Number(data[i].trim());
-    if (!isNaN(num)) {
-      sum += num;
+    if (!isNaN(data[i])) {
+      sum += data[i];
+      count++;
     }
   }
-  return sum;
+  return sum / count;
 }
+console.log(sumNumbers(arr));
 
 // Write the function doMath(x, znak, y), which receives 3 arguments: the numbers x and y, the string znak. The znak variable can be: +, -, *, /, %, ^ (degree).
 // Output the result of the mathematical operation specified in the znak variable. Both numbers and the mathematical operator come from the user.
@@ -18,6 +21,9 @@ function sumNumbers() {
 function doMath() {
   let x = Number(prompt("Enter first number"));
   let y = Number(prompt("Enter second number"));
+  if (!isNaN(x) || !isNaN(y)) {
+    return alert("Incorrect type of entered value");
+  }
   let znak = prompt("Enter mathematical operator");
   switch (znak) {
     case "+":
@@ -32,6 +38,8 @@ function doMath() {
     case "/":
       return x / y;
       break;
+    default:
+      alert("Incorrect math operator");
   }
 }
 
@@ -42,32 +50,30 @@ function fillTwoDimensionalArray() {
   let masterArray = [];
   let masterArrayLength = prompt("Specify the main array length");
 
-  for (let i = 0; i < masterArrayLength; i++) {
+  function fillTheChildArray(value) {
     let childArray = [];
-    for (let j = 0; j < masterArrayLength; j++) {
-      let inputValue = prompt(
-        `Enter value for ${j + 1} child array in row ${i + 1}`
-      );
-      if (inputValue === null) {
-        break;
-      }
-      let parsedValue = parseFloat(inputValue);
-      if (!isNaN(parsedValue)) {
-        childArray.push(parsedValue);
-      }
+    let childArrayLength = prompt(
+      `Specify the ${value + 1} child array length`
+    );
+    let inputChildArray = prompt(
+      `Enter values for ${value + 1} child array, separating them with a coma`
+    ).split(",");
+    if (Number(childArrayLength) === inputChildArray.length) {
+      childArray.push(inputChildArray);
     }
-    masterArray.push(childArray);
+    return childArray;
   }
 
+  for (let i = 0; i < masterArrayLength; i++) {
+    masterArray.push(fillTheChildArray(i));
+  }
   return masterArray;
 }
 
 // Create a function that removes from the string all the characters that we passed with the second argument.
 // 'func(" hello world", ['l', 'd'])' will return us "heo wor". The output string and characters to remove are specified by the user.
 
-function removeCharsFromString() {
-  let str = prompt("Enter string");
-  let charsToRemove = prompt("Enter list of symbols").split(",");
+function removeCharsFromString(str, charsToRemove) {
   let result = "";
   for (let i = 0; i < str.length; i++) {
     if (!charsToRemove.includes(str[i])) {
@@ -76,3 +82,4 @@ function removeCharsFromString() {
   }
   return result;
 }
+console.log(removeCharsFromString(" hello world", ["l", "d"]));
