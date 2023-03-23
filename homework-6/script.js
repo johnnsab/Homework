e7ddc837eed149961117a227f0a166a453c6fc04
johnnsab <sabranjohn@gmail.com>
2023-03-23 13:15:56 +0200

    first commit

---
+++ homework-6/script.js
@@ -0,0 +1,20 @@
+// Write a function that accepts 1 parameter and sums up with what was passed the first time, etc. All this with closures
+
+function makeSum() {
+  let num = 0;
+  return function add(value) {
+    return (num += value);
+  };
+}
+const sum = makeSum();
+
+// Using a closure, write the structureUserInfo function, which collects the user info object from the arguments.
+
+function UserInfo(name) {
+  return function (role) {
+    return {
+      name: name,
+      role: role,
+    };
+  };
+}
